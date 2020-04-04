c87917edc806228733653a71e4e904d2cbff9c31
hzw0407 <624490352@qq.com>
2020-04-04 17:32:33 +0800

    新闻列表

--- helloworld/Test/Mine.js
+++ helloworld/Test/Mine.js
@@ -1,41 +1,123 @@
 import React,{Component} from 'react';
-import {View,Text,StyleSheet,Image, Alert} from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image, 
+    Alert,
+FlatList} from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { HeaderTitle } from 'react-navigation-stack';
 import {fetchRequest} from './NetworkRequest'
 
 export default class Mine extends React.Component {
     static navigationOptions = ({ navigation, navigationOptions }) => ({
-        title:navigation.state.params?navigation.state.params.headerTitle:'我的',
+        title:navigation.state.params?navigation.state.params.headerTitle:'新闻列表',
         gestureResponseDistance:{horizontal:300},
     });
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            //数据数组
+            infoData: [],
+        };
+        this.getListInfo = this.getListInfo.bind(this);
+    }
+
     componentDidMount() {
-        fetchRequest('http://facebook.github.io/react-native/movies.json','GET')
+        this.getListInfo();
+    }
+
+    //获取新闻列表数据
+    getListInfo() {
+        fetchRequest('http://v.juhe.cn/toutiao/index?key=d8edd44350a7dcf5f54ce7fdbd7699b3&type=top','GET')
         .then(res => {
           //请求成功
-        //   Alert.alert('请求成功');
+          this.setState({
+              infoData:this.state.infoData.concat(res.result.data),
+          });
         }).catch(err => {
           //请求失败
-        //   Alert.alert('请求失败');
+          Alert.alert('请求失败');
         });
     }
 
-    render() {
+    renderInfo({item}) {
         return (
             <View style={styles.container}>
-              <Text style={styles.text}>我的123</Text>
-          </View>
-        )
+                <Image style={styles.imageStyle} source={{uri:item.thumbnail_pic_s}}></Image>
+                <View style={styles.infoViewStyle}>
+                   <Text style={styles.title}>{'标题:' + item.title}</Text>
+                   <Text style={styles.authorStyle}>{'作者:' + item.author_name}</Text>
+                   <View style={styles.dateViewStyle}>
+                       <Text style={styles.dateStyle}>{'发布日期:' + item.date}</Text>
+                   </View>
+                </View>
+            </View>
+        );
+    }
+
+    render() {
+        // return (
+        //     <View style={styles.container}>
+        //       <Text style={styles.text}>我的123</Text>
+        //   </View>
+        // )
+        return (
+            <FlatList
+                data={this.state.infoData}
+                renderItem={this.renderInfo}
+                style={styles.list}
+                keyExtractor={item => item.index}
+            />
+        );
     }
 }
 
 const styles=StyleSheet.create({
     container:{
         flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#f5f5f5'
+        flexDirection:"row",
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"#f5f5f5"
+    },
+    imageStyle:{
+        flex:1,
+        justifyContent:"flex-start",
+        marginLeft:10,
+        marginBottom:10,
+        height:150,
+    },
+    infoViewStyle:{
+        flex:1,
+        flexDirection:"column",
+        justifyContent:"flex-start",
+        // alignItems:"flex-start",
+        marginTop:0,
+        marginBottom:10,
+        marginLeft:10,
+        marginRight:10,
+        backgroundColor:"#00ffff",
+        height:150,
+    },
+    titleStyle:{
+        fontSize:16,
+    },
+    authorStyle:{
+        marginTop:10,
+        fontSize:14,
+    },
+    dateViewStyle:{
+        flex:1,
+        flexDirection:"row",
+        justifyContent:"flex-end",
+        alignItems:"flex-end",
+        // backgroundColor:"#ff1493",
+    },
+    dateStyle:{
+        fontSize:12,
     },
     text:{
         fontSize:26
